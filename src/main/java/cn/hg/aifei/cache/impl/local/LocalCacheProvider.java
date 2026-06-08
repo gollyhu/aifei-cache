1a1d2149beb66262d0a563c9d877120d17cfd729
gollyhu <3722711@qq.com>
2026-06-08 12:24:31 +0800

    修改版权署名

--- src/main/java/cn/hg/aifei/cache/impl/local/LocalCacheProvider.java
+++ src/main/java/cn/hg/aifei/cache/impl/local/LocalCacheProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2035 gollyhu (https://github.com/gollyhu)
+ * Copyright 2021-2035 糊搞 (https://github.com/gollyhu)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
